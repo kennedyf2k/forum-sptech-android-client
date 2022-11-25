76406e416f25395f6b4023ca9531d229b52b7d25
carolinebeirao <carolinebeirao@outlook.com>
2022-11-25 19:16:06 -0300

    bora carolina costa deixa de ser preguicosa

--- app/src/main/java/br/com/studenton/fragments/FeedFragment.kt
+++ app/src/main/java/br/com/studenton/fragments/FeedFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import androidx.recyclerview.selection.SelectionTracker
 import androidx.recyclerview.selection.StorageStrategy
@@ -193,11 +194,15 @@ class FeedFragment : Fragment() {
 
                             }else {
 
+
+
                                 adapterPublicacoes.setData(response.body()!!)
 
                                 binding.shimmerFrameLayout.stopShimmerAnimation()
                                 binding.shimmerFrameLayout.visibility = View.GONE
                                 binding.llImgSemPublicacao.visibility = View.GONE
+                                binding.feedSelectedRigth.visibility = View.INVISIBLE
+                                binding.feedSelectedLeft.visibility = View.INVISIBLE
 
                                 rvFeed.visibility = View.VISIBLE
 
@@ -230,6 +235,9 @@ class FeedFragment : Fragment() {
                             response: Response<MutableList<Publicacao>>
                         ) {
 
+                            binding.feedSelectedRigth.visibility = View.VISIBLE
+                            binding.feedSelectedLeft.visibility = View.INVISIBLE
+
                             binding.shimmerFrameLayout.visibility = View.VISIBLE
                             binding.shimmerFrameLayout.startShimmerAnimation()
 
@@ -261,6 +269,9 @@ class FeedFragment : Fragment() {
                             call: Call<MutableList<Publicacao>>,
                             response: Response<MutableList<Publicacao>>
                         ) {
+
+                            binding.feedSelectedRigth.visibility = View.INVISIBLE
+                            binding.feedSelectedLeft.visibility = View.VISIBLE
 
                             binding.shimmerFrameLayout.visibility = View.VISIBLE
                             binding.shimmerFrameLayout.startShimmerAnimation()
