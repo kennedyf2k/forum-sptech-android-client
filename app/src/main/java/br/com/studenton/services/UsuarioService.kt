89b8717f2fbfc66f5d276191d32a432b0285dce6
carolinebeirao <carolinebeirao@outlook.com>
2022-12-02 14:09:20 -0300

    erro serviceUsuario resolved

--- app/src/main/java/br/com/studenton/services/UsuarioService.kt
+++ app/src/main/java/br/com/studenton/services/UsuarioService.kt
@@ -1,11 +1,9 @@
 package br.com.studenton.services
 
 import br.com.studenton.domain.Login
-<<<<<<< HEAD
-=======
 import br.com.studenton.domain.Publicacao
 import br.com.studenton.domain.request.EsqueceuSenhaRequest
->>>>>>> fe7ea54378e3a37e86aa80f3792077c2784d5b8d
+
 import br.com.studenton.domain.request.TrocarSenhaRequest
 import retrofit2.Call
 import retrofit2.http.*
@@ -18,12 +16,13 @@ interface UsuarioService {
     @GET("/usuarios")
     fun getAllUsuarios(): Call<MutableList<Login>>
 
-<<<<<<< HEAD
-    @PATCH("usuarios/mudar-perfil")
-    fun trocarFotoPerfil(@Query("id") id: Int, @Query("perfil") trocarPerfilRequest: String): Call<Unit>
-=======
     @PATCH("usuarios/esqueci-senha")
     fun esqueciSenha(@Body body: EsqueceuSenhaRequest): Call<Unit>
->>>>>>> fe7ea54378e3a37e86aa80f3792077c2784d5b8d
+
+    @PATCH("usuarios/mudar-perfil")
+    fun trocarFotoPerfil(@Query("id") id: Int, @Query("perfil") trocarPerfilRequest: String): Call<Unit>
+
+
+
 
 }
